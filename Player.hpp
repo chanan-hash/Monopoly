aa3fd3a78d7a48064e2afb22a18ec605413c6258
chanan-hash <chananhelman@gmail.com>
2024-09-11 09:27:15 +0300

    first commit

---
+++ Player.hpp
@@ -0,0 +1,11 @@
+/**
+ * This class represnts a simple player in the game
+ * Each player has a name, money, card, and assets (streets, train stations, and utilities)
+ */ 
+
+#include<iostream>
+#include<string>
+#include<vector>
+
+class Player
+{};
