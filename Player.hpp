ba5e2d76616aa322a5ed5758bc6d1cad04a09282
chanan-hash <chananhelman@gmail.com>
2024-09-11 22:32:03 +0300

    update

--- Player.hpp
+++ Player.hpp
@@ -7,5 +7,34 @@
 #include<string>
 #include<vector>
 
+using namespace std;
+
 class Player
-{};
+{
+    private:
+        string name;
+        int money;
+        vector<supriseCard> card; // need to be implemented
+        vector<assets> assets; // need to be implemented
+
+    public:
+        Player(string name);
+        void addMoney(int amount); // we can add a negative amount to remove money
+        void removeMoney(int amount);
+        void addCard(supriseCard card); // need to be implemented
+        void removeCard(supriseCard card); // need to be implemented
+        void addAsset(assets asset); // need to be implemented
+        void removeAsset(assets asset); // need to be implemented
+
+        // getters
+        string getName();
+        int getMoney();
+        vector<supriseCard> getCard(); // need to be implemented
+        vector<assets> getAssets(); // need to be implemented
+        void printPlayer();
+        friend ostream& operator<<(ostream& os, const Player& player);
+
+        
+
+};
+
