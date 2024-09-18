63a37f40bd26a42115d444fe04d2fb95fbd201f1
chanan-hash <chananhelman@gmail.com>
2024-09-18 14:47:07 +0300

    adding more tests

--- Test/test.cpp
+++ Test/test.cpp
@@ -4,6 +4,7 @@
 #include "../Player.hpp"
 #include "../Cards/supriseCard.hpp"
 #include "../Cards/AdvancedToGo.hpp"
+#include "../Cards/AdvancedToBoardWalk.hpp"
 
 using namespace std;
 TEST_CASE("Cards Testing")
@@ -22,18 +23,16 @@ TEST_CASE("Cards Testing")
     players.push_back(&player2);
     players.push_back(&player3);
 
-    //Setting the players position to 0
+    // Setting the players position to 0
     player1.setPosition(0);
     player2.setPosition(0);
     player3.setPosition(0);
-
 
     // Checking that the position of the players is 0
     for (size_t i = 0; i < players.size(); i++)
     {
         CHECK(players[i]->getPosition() == 0);
     }
-    
 
     // Adding the players to the first slot
     board.getBoard()[0]->addPlayer(player1);
@@ -49,7 +48,7 @@ TEST_CASE("Cards Testing")
     SUBCASE("Advance to Go (Collect $200)")
     {
         // Creating the card
-            AdvancedToGo advancedToGo;
+        AdvancedToGo advancedToGo;
 
         // putting plyer1 another slot
         board.getBoard()[player1.getPosition()]->removePlayer(player1);
@@ -69,5 +68,22 @@ TEST_CASE("Cards Testing")
 
         // Checking that the player is on the first slot
         CHECK(board.getBoard()[0]->getPlayers()[2].getName() == player1.getName());
+    }
+
+    SUBCASE("Checking AdvancedToBoardWalk")
+    {
+        // Creating the card
+        AdvancedToBoardWalk advancedToBoardWalk;
+        
+        // moving player2 to the last slot
+        advancedToBoardWalk.action(player2, board);
+        cout << "player2 position: " << player2.getPosition() << endl;
+
+        // Checking that the player is on the last slot
+        CHECK(player2.getPosition() == 39);
+
+        // cheking that this slot player is not empty
+        CHECK_FALSE(board.getBoard()[39]->getPlayers().empty());
+        CHECK(board.getBoard()[39]->getPlayers()[0].getName() == player2.getName());
     }
 }
