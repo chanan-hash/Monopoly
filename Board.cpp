364c44f8be76913701cebc7b8cae938bd6d065ca
chanan-hash <chananhelman@gmail.com>
2024-09-26 17:50:38 +0300

    Adding scrolling bar

--- Board.cpp
+++ Board.cpp
@@ -128,6 +128,9 @@ std::ostream &operator<<(std::ostream &os, const Board &board)
     return os;
 }
 
+/**
+ * The GUI is not part of the class, we've put it here for faster compilation
+ */
 // GUI-related functions
 void initializeSlots(const Board &board, std::vector<sf::RectangleShape> &slotShapes, std::vector<sf::Text> &slotTexts, sf::Font &font)
 {
@@ -205,6 +208,35 @@ void draw(sf::RenderWindow &window, const std::vector<sf::RectangleShape> &slotS
     }
 }
 
+// void runGUI(const Board &board)
+// {
+//     sf::RenderWindow window(sf::VideoMode(800, 800), "Monopoly Board");
+//     sf::Font font;
+//     if (!font.loadFromFile("Arimo-Italic-VariableFont_wght.ttf"))
+//     {
+//         std::cerr << "Error loading font" << std::endl;
+//         return;
+//     }
+
+//     std::vector<sf::RectangleShape> slotShapes;
+//     std::vector<sf::Text> slotTexts;
+//     initializeSlots(board, slotShapes, slotTexts, font);
+
+//     while (window.isOpen())
+//     {
+//         sf::Event event;
+//         while (window.pollEvent(event))
+//         {
+//             if (event.type == sf::Event::Closed)
+//                 window.close();
+//         }
+
+//         window.clear(sf::Color::White);
+//         draw(window, slotShapes, slotTexts);
+//         window.display();
+//     }
+// }
+
 void runGUI(const Board &board)
 {
     sf::RenderWindow window(sf::VideoMode(800, 800), "Monopoly Board");
@@ -219,6 +251,17 @@ void runGUI(const Board &board)
     std::vector<sf::Text> slotTexts;
     initializeSlots(board, slotShapes, slotTexts, font);
 
+    // Scrollbar properties
+    sf::RectangleShape scrollbar(sf::Vector2f(20, 100));
+    scrollbar.setPosition(780, 0);
+    scrollbar.setFillColor(sf::Color(200, 200, 200));
+    scrollbar.setOutlineThickness(1);
+    scrollbar.setOutlineColor(sf::Color::Black);
+
+    sf::View view = window.getDefaultView();
+    bool isDragging = false;
+    float lastMouseY = 0;
+
     while (window.isOpen())
     {
         sf::Event event;
@@ -226,92 +269,56 @@ void runGUI(const Board &board)
         {
             if (event.type == sf::Event::Closed)
                 window.close();
+            else if (event.type == sf::Event::MouseWheelScrolled)
+            {
+                if (event.mouseWheelScroll.delta > 0)
+                {
+                    view.move(0, -20); // Scroll up
+                }
+                else
+                {
+                    view.move(0, 20); // Scroll down
+                }
+                window.setView(view);
+            }
+            else if (event.type == sf::Event::MouseButtonPressed)
+            {
+                if (event.mouseButton.button == sf::Mouse::Left)
+                {
+                    if (scrollbar.getGlobalBounds().contains(event.mouseButton.x, event.mouseButton.y))
+                    {
+                        isDragging = true;
+                        lastMouseY = event.mouseButton.y;
+                    }
+                }
+            }
+            else if (event.type == sf::Event::MouseButtonReleased)
+            {
+                if (event.mouseButton.button == sf::Mouse::Left)
+                {
+                    isDragging = false;
+                }
+            }
+            else if (event.type == sf::Event::MouseMoved)
+            {
+                if (isDragging)
+                {
+                    float deltaY = event.mouseMove.y - lastMouseY;
+                    scrollbar.move(0, deltaY);
+                    lastMouseY = event.mouseMove.y;
+
+                    // Adjust view based on scrollbar position
+                    float viewMoveY = deltaY * (700.0f / window.getSize().y); // Adjust this factor as needed
+                    view.move(0, viewMoveY);
+                    window.setView(view);
+                }
+            }
         }
 
         window.clear(sf::Color::White);
+        window.setView(view);
         draw(window, slotShapes, slotTexts);
+        window.draw(scrollbar);
         window.display();
     }
 }
-
-// GUI-related functions
-// void initializeSlots(const Board &board, std::vector<sf::RectangleShape> &slotShapes, std::vector<sf::Text> &slotTexts, sf::Font &font) {
-//     const int BOARD_SIZE = 700;
-//     const int SLOT_COUNT = 40;
-//     float slotWidth = BOARD_SIZE / 11.0f;
-//     float slotHeight = BOARD_SIZE / 11.0f;
-
-//     for (int i = 0; i < SLOT_COUNT; ++i) {
-//         sf::RectangleShape shape(sf::Vector2f(slotWidth, slotHeight));
-//         sf::Text text("", font, 10);
-
-//         // Position the slots
-//         if (i < 11) {
-//             shape.setPosition((10 - i) * slotWidth + 50, BOARD_SIZE - slotHeight + 50);
-//         } else if (i < 21) {
-//             shape.setPosition(50, (20 - i) * slotHeight + 50);
-//         } else if (i < 31) {
-//             shape.setPosition((i - 20) * slotWidth + 50, 50);
-//         } else {
-//             shape.setPosition(BOARD_SIZE - slotWidth + 50, (i - 30) * slotHeight + 50);
-//         }
-
-//         // Set color based on property color
-//         Slot* slot = board.getBoard()[i];
-//         sf::Color color = sf::Color::White;
-//         if (Streets* street = dynamic_cast<Streets*>(slot)) {
-//             std::string propertyColor = street->getColor();
-//             if (propertyColor == "Brown") color = sf::Color(165, 42, 42);
-//             else if (propertyColor == "Light Blue") color = sf::Color(173, 216, 230);
-//             else if (propertyColor == "Pink") color = sf::Color(255, 192, 203);
-//             else if (propertyColor == "Orange") color = sf::Color(255, 165, 0);
-//             else if (propertyColor == "Red") color = sf::Color::Red;
-//             else if (propertyColor == "Yellow") color = sf::Color::Yellow;
-//             else if (propertyColor == "Green") color = sf::Color::Green;
-//             else if (propertyColor == "Dark Blue") color = sf::Color::Blue;
-//         }
-//         shape.setFillColor(color);
-//         shape.setOutlineThickness(1);
-//         shape.setOutlineColor(sf::Color::Black);
-
-//         // Set text
-//         text.setString(slot->getName());
-//         text.setFillColor(sf::Color::Black);
-//         text.setPosition(shape.getPosition() + sf::Vector2f(5, 5));
-
-//         slotShapes.push_back(shape);
-//         slotTexts.push_back(text);
-//     }
-// }
-
-// void draw(sf::RenderWindow &window, const std::vector<sf::RectangleShape> &slotShapes, const std::vector<sf::Text> &slotTexts) {
-//     for (size_t i = 0; i < slotShapes.size(); ++i) {
-//         window.draw(slotShapes[i]);
-//         window.draw(slotTexts[i]);
-//     }
-// }
-
-// void runGUI(const Board &board) {
-//     sf::RenderWindow window(sf::VideoMode(800, 800), "Monopoly Board");
-//     sf::Font font;
-//     if (!font.loadFromFile("arial.ttf")) {
-//         std::cerr << "Error loading font" << std::endl;
-//         return;
-//     }
-
-//     std::vector<sf::RectangleShape> slotShapes;
-//     std::vector<sf::Text> slotTexts;
-//     initializeSlots(board, slotShapes, slotTexts, font);
-
-//     while (window.isOpen()) {
-//         sf::Event event;
-//         while (window.pollEvent(event)) {
-//             if (event.type == sf::Event::Closed)
-//                 window.close();
-//         }
-
-//         window.clear(sf::Color::White);
-//         draw(window, slotShapes, slotTexts);
-//         window.display();
-//     }
-// }
